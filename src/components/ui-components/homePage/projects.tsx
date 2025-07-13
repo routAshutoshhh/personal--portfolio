d213faa0e82571e038fec0cb96cf1a55ebf26d38
routAshutoshhh <egoisticdev@gmail.com>
2025-07-13 21:07:57 +0530

    projects added

--- src/components/ui-components/homePage/projects.tsx
+++ src/components/ui-components/homePage/projects.tsx
@@ -5,41 +5,47 @@ import { cn } from "@/lib/utils"; // Optional utility for conditional classNames
 
 const projects = [
   {
-    title: "Cypher Chat",
+    title: "CodeOmen",
     description:
-      "Secure client-server chat web app enabling real-time and private communication.",
-    tech: ["TypeScript", "React", "Next.js", "Node.js", "Express.js", "AWS"],
+      "A Coding platform  with user interface just like Leetcode to test your skills in  Data Structure and algo .",
+    tech: [
+      "Next.js ",
+      "TypeScript",
+      "Node.js",
+      "Express.js",
+      "Firestore",
+      "Firebase Authentication",
+      "codeMirror",
+    ],
   },
   {
-    title: "Sole Exchange",
+    title: "Todo web Application ",
     description:
-      "Forum that allows users to post, interact, and search about anything sneaker related.",
-    tech: ["React", "JavaScript", "Node.js", "Supabase"],
-  },
-  {
-    title: "Spotify Data Dashboard",
-    description:
-      "Dashboard that fetches Spotify's Top 50 songs and visualizes data in text and chart form.",
-    tech: ["React", "Spotify API", "JavaScript", "Node.js"],
+      "A todo web application which leverages local storage with simple user interface  to just say everyday task.",
+    tech: ["React", "Context API", "JavaScript"],
   },
 ];
 
 const techColors: Record<string, string> = {
   TypeScript: "bg-blue-500",
   React: "bg-cyan-500",
-  JavaScript: "bg-yellow-400 text-black",
+  JavaScript: "bg-yellow-400",
   "node.js": "bg-green-500",
   "Next.js": "bg-neutral-700",
   "Express.js": "bg-orange-400",
   AWS: "bg-orange-500",
   Supabase: "bg-emerald-500",
   "Spotify API": "bg-emerald-600",
+  "Context API": "bg-purple-500",
+  Firestore: "bg-blue-800",
+  "Firebase Authentication": "bg-yellow-500",
+  codeMirror: "bg-red-300",
 };
 
 const ProjectsSection = () => {
   return (
-    <section className="mt-12 flex flex-col sm:flex-row border border-red-700">
-      <h2 className="text-lg sm:text-xl font-medium tracking-tight border border-red-300">
+    <section className="mt-12 flex flex-col sm:flex-row ">
+      <h2 className="text-lg sm:text-xl font-medium tracking-tight">
         Projects
       </h2>
       <div className="ml-6 space-y-8">
