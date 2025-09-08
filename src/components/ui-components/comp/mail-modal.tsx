15fa34338008e29576c24553aa9f25eaf9c6dc2a
routAshutoshhh <egoisticdev@gmail.com>
2025-09-08 21:15:57 +0530

    backend integrated

--- src/components/ui-components/comp/mail-modal.tsx
+++ src/components/ui-components/comp/mail-modal.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import React,{useState} from 'react';
-import { Button } from "@/components/ui/button"
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -9,30 +9,28 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useModalStore } from "@/stores/modalStore"
-import {z} from "zod";
-import {BiMailSend} from "react-icons/bi";
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useModalStore } from "@/stores/modalStore";
+import { z } from "zod";
+import { BiMailSend } from "react-icons/bi";
 import { FcCancel } from "react-icons/fc";
 import axios from "axios";
-import { toast } from 'react-hot-toast';
-import {MoonLoader} from "react-spinners"
-
-
+import { toast } from "react-hot-toast";
+import { MoonLoader } from "react-spinners";
 
 //work with zod validatation for schema
 //defining the shema first using zod
 const schemaMail = z.object({
-    name: z.string().min(1, "Name is required"),
-    email: z.string().email("Invalid email address"),
-    subject: z.string().optional(),
-    message: z.string().min(20, "message  must be of atleast 20 characters"),
-   // attachment: z.instanceof(File).optional()
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  subject: z.string().optional(),
+  message: z.string().min(20, "message  must be of atleast 20 characters"),
+  // attachment: z.instanceof(File).optional()
 });
 
-//defining the type for the schema for tyepscript 
+//defining the type for the schema for tyepscript
 type emailSchemaType = z.infer<typeof schemaMail>;
 
 export function MailModal() {
@@ -40,46 +38,48 @@ export function MailModal() {
   const { isOpen, closeModal } = useModalStore();
 
   //state for submitingt the form to be set as true when clicked the send button and false otherwise
-  const [isSubmitClicked  , setIsSubmitClicked] = useState<boolean>(false);
+  const [isSubmitClicked, setIsSubmitClicked] = useState<boolean>(false);
 
   //state for the loading for the form submission
-  const [mailLoader , setMailLoader ] = useState<boolean>(false);
-
+  const [mailLoader, setMailLoader] = useState<boolean>(false);
 
   //state for the form data
-  const [formData , setFormData] = useState<emailSchemaType>({
-    name:"",
-    email:"",
-    subject:"",
-    message:""
-  })
+  const [formData, setFormData] = useState<emailSchemaType>({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
 
   const validateField = (name: keyof emailSchemaType, value: string) => {
-  try {
-    schemaMail.shape[name].parse(value); // validate only this field
-    setErrors((prev) => ({ ...prev, [name]: "" })); // clear error if valid
-  } catch (err: any) {
-    setErrors((prev) => ({ ...prev, [name]: err.message })); // set error if invalid
-  }
-};
-
+    try {
+      schemaMail.shape[name].parse(value); // validate only this field
+      setErrors((prev) => ({ ...prev, [name]: "" })); // clear error if valid
+    } catch (err: any) {
+      setErrors((prev) => ({ ...prev, [name]: err.message })); // set error if invalid
+    }
+  };
 
   //state for the form errors - todo:loook into this more deeply later
-  const[errors , setErrors] = useState<Partial<Record<keyof emailSchemaType, string>>>({});
+  const [errors, setErrors] = useState<
+    Partial<Record<keyof emailSchemaType, string>>
+  >({});
 
-  //hamdling the changing  in the input - we neec to fire up a event handler to handle change 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
-    const {name , value } = e.target;
+  //hamdling the changing  in the input - we neec to fire up a event handler to handle change
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
     const key = name as keyof emailSchemaType;
 
     setIsSubmitClicked(false); // resetting the submit clicked to false
-    setFormData((prev) => ({...prev , [name]:value}))
+    setFormData((prev) => ({ ...prev, [name]: value }));
     validateField(key, value); // validate the field on change
-  }
+  };
 
   //handling the form submission - a form event handler
-  const handleSubmit = async(e: React.FormEvent) =>{
-    console.log("buttton clicked")
+  const handleSubmit = async (e: React.FormEvent) => {
+    console.log("buttton clicked");
     setIsSubmitClicked(true); //setting the submit clicked to true
     setMailLoader(true); //now sending the mail has starter
     e.preventDefault();
@@ -88,56 +88,59 @@ export function MailModal() {
     const result = schemaMail.safeParse(formData);
 
     //validation failed
-    if(!result.success){
-        
-        const feildErrors: typeof errors = {};
-        //iterating over the errors and setting them in the state
-        const zodError = result.error;
-        //zodError.errors is an array of errors, we need to iterate over it        
-        zodError.issues .forEach((err :any) => {
-            //error.path is an array of strings, we need to join them to get the key
-            const key = err.path[0] as keyof  emailSchemaType;
-            feildErrors[key] = err.message; //setting the error message
-        });
+    if (!result.success) {
+      const feildErrors: typeof errors = {};
+      //iterating over the errors and setting them in the state
+      const zodError = result.error;
+      //zodError.errors is an array of errors, we need to iterate over it
+      zodError.issues.forEach((err: any) => {
+        //error.path is an array of strings, we need to join them to get the key
+        const key = err.path[0] as keyof emailSchemaType;
+        feildErrors[key] = err.message; //setting the error message
+      });
 
-        setErrors(feildErrors);
+      setErrors(feildErrors);
 
-        //if we have errors then we need to stop the loading
-        setMailLoader(false);
-        toast.error("please fill the form corrrectly ")
-        return;
+      //if we have errors then we need to stop the loading
+      setMailLoader(false);
+      toast.error("please fill the form corrrectly ");
+      return;
     }
 
     //if validation passed we neeed to set the errror empty
     setErrors({});
-    //adding the api call to send the message 
+    //adding the api call to send the message
+    const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+    try {
+      const res = await axios.post(`${API_URL}/sendEmail`, formData);
+      console.log("Response from the server: ", res.data);
 
-    try{
-        const res  =await axios.post("/api/mail", formData);
-        console.log("Response from the server: ", res.data)
-
-        //if the response is anything else than 200 , its a fail for us
-        if(res.status != 200){
-            toast.error("email send failed due to some error - please send a mail using gmail at the same email address");
-            throw new Error("Failed to send Email");
-        }
-        //this is the success case
-        toast.success("Email sent successfully!");
-       // alert("email sent successfully")
-       //clearing the form data after the successful submisssion
-       setFormData({
-        name:"",
-        email:"",
-        subject:"",
-        message:""
-       })
-    }catch(err){
-        toast.error("email send failed due to some error - please send a mail using gmail at the same email address");
-        console.error(err);
+      //if the response is anything else than 200 , its a fail for us
+      if (res.status != 200) {
+        toast.error(
+          "email send failed due to some error - please send a mail using gmail at the same email address"
+        );
+        throw new Error("Failed to send Email from the backend");
+      }
+      //this is the success case
+      toast.success("Email sent successfully!");
+      // alert("email sent successfully")
+      //clearing the form data after the successful submisssion
+      setFormData({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+    } catch (err) {
+      toast.error(
+        "email send failed due to some error - please send a mail using gmail at the same email address"
+      );
+      console.error(err);
     }
     setMailLoader(false);
     closeModal(); // after the submission is done we can may be close the modal
-  }
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && closeModal()}>
@@ -146,76 +149,81 @@ export function MailModal() {
           <DialogTitle>Email me!</DialogTitle>
         </DialogHeader>
 
-        {/*putting everthing inside a form for submission */}    
-    <form onSubmit={handleSubmit}>
-      <div className="grid gap-4 overflow-auto">
-        <div className="grid gap-3">
-          <Label htmlFor="name">Name</Label>
-          <Input
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Your full Name"
-          />
-          {(isSubmitClicked&&errors.name) && <p className="text-red-500 text-sm mt-1">*{errors.name}</p>}
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Your email address"
-          />
-          {(isSubmitClicked && errors.email) && <p className="text-red-500 text-sm mt-1">*{errors.email}</p>}
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="subject">Subject</Label>
-          <Input
-            id="subject"
-            name="subject"
-            value={formData.subject}
-            onChange={handleChange}
-          />
-          
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="content">Message</Label>
-          <textarea
-            id="content"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Type your message here"
-            className="min-h-[100px] rounded-md border px-3 py-2 text-sm shadow-sm" // if not styled, add Tailwind here
-          />
-          {(isSubmitClicked && errors.message) && <p className="text-red-500 text-sm mt-1">*{errors.message}</p>}
-        </div>
-      </div>
+        {/*putting everthing inside a form for submission */}
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 overflow-auto">
+            <div className="grid gap-3">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Your full Name"
+              />
+              {isSubmitClicked && errors.name && (
+                <p className="text-red-500 text-sm mt-1">*{errors.name}</p>
+              )}
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Your email address"
+              />
+              {isSubmitClicked && errors.email && (
+                <p className="text-red-500 text-sm mt-1">*{errors.email}</p>
+              )}
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="subject">Subject</Label>
+              <Input
+                id="subject"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="content">Message</Label>
+              <textarea
+                id="content"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Type your message here"
+                className="min-h-[100px] rounded-md border px-3 py-2 text-sm shadow-sm" // if not styled, add Tailwind here
+              />
+              {isSubmitClicked && errors.message && (
+                <p className="text-red-500 text-sm mt-1">*{errors.message}</p>
+              )}
+            </div>
+          </div>
 
-      <DialogFooter className="mt-4">
-        <DialogClose asChild>
-          <Button variant="outline">
-            <FcCancel className="m-1" />
-          </Button>
-        </DialogClose>
-        <Button type="submit">
-            {mailLoader? (
-                <div className='flex items-center justify-center'>
-                    <MoonLoader size={15} color={"#4583d5"}/>
-                </div>  
-                ) : (
-                    <>
-                    <BiMailSend className="mr-1" />
-                    Send
-                    </>
-                )}
-        </Button>
-      </DialogFooter>
-    </form>
-  </DialogContent>
-</Dialog>
-  )
+          <DialogFooter className="mt-4">
+            <DialogClose asChild>
+              <Button variant="outline">
+                <FcCancel className="m-1" />
+              </Button>
+            </DialogClose>
+            <Button type="submit">
+              {mailLoader ? (
+                <div className="flex items-center justify-center">
+                  <MoonLoader size={15} color={"#4583d5"} />
+                </div>
+              ) : (
+                <>
+                  <BiMailSend className="mr-1" />
+                  Send
+                </>
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 }
